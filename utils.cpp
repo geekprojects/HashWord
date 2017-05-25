26a1d0e453b7b6f859133341e322f507686bab64
Ian Parker <ian@geekprojects.com>
2017-05-25 14:29:57 +0100

    Utils: Use platform independant PRIx64 for 64 bit numbers

--- utils.cpp
+++ utils.cpp
@@ -3,13 +3,15 @@
 #include <stdint.h>
 #include <ctype.h>
 
+#include <cinttypes>
+
 void hexdump(const char* pos, int len)
 {
     int i;
     for (i = 0; i < len; i += 16)
     {
         int j;
-        printf("%08llx: ", (uint64_t)(pos + i));
+        printf("%08" PRIx64 ": ", (uint64_t)(pos + i));
         for (j = 0; j < 16 && (i + j) < len; j++)
         {
             printf("%02x ", (uint8_t)pos[i + j]);
